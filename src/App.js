98f2d8d84d94b723d3cca9fd5715d9f4cf622e01
viniciusClunc <vinicius@valorizza.com.br>
2022-10-18 17:31:54 -0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,109 @@
-import Header from './components/Header';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+
+import { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(true)
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+
+  // Buscando as tarefas do backEnd
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = res.json()
+
+    return data
+  }
+
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = res.json()
+
+    return data
+  }
+
+
+  // Adicionando uma tarefa
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = {
+    //   id,
+    //   ...task
+    // }
+    // setTasks([...tasks, newTask])
+  }
+
+
+  // Excluindo uma tarefa
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+
+  // Alternar Reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task) => {
+        return task.id === id ? { ...task, reminder: data.reminder } : task;
+      })
+    );
+  };
 
   return (
     <div className="container">
-      <Header title="Hello" />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "Sem tarefas para mostrar"
+      )}
+      <Footer />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
+
   return (
     <header className="header">
-      <h1>{title}</h1>      
-      <Button color='green' text='Adicionar Tarefa' />
+      <h1>{title}</h1>
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Fechar' : 'Adicionar'} onClick={onAdd} />
     </header>
   );
 };
